5eb7663e9b2ab1e0d69d4e80bf2439c6ad6719de
naveen <naveenkumar9707@gmail.com>
2024-06-22 16:39:01 -0500

    Roboshop servers

---
+++ roboshop/servers.tf
@@ -0,0 +1,70 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "frontend"
+  }
+}
+resource "aws_instance" "mongod" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "mongod"
+  }
+}
+resource "aws_instance" "catalogue" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "catalogue"
+  }
+}
+resource "aws_instance" "redis" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "redis"
+  }
+}
+resource "aws_instance" "user" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "user"
+  }
+}
+resource "aws_instance" "cart" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "cart"
+  }
+}
+resource "aws_instance" "mysql" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "mysql"
+  }
+}
+resource "aws_instance" "shipping" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "shipping"
+  }
+}
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "rabbitmq"
+  }
+}
+resource "aws_instance" "payment" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name= "payment"
+  }
+}
