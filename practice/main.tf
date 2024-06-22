dc711c231dbb9761c125680abf86b0e965a3cd84
naveen <naveenkumar9707@gmail.com>
2024-06-22 15:51:59 -0500

    Terraform practice

--- practice/main.tf
+++ practice/main.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "testserver" {
   ami = "ami-0b4f379183e5706b9"
   spot_price    = "0.03"
-  instance_type = "t4g.nano"
+  instance_type = "t3.micro"
   tags = {
     Name= "naveen"
   }
