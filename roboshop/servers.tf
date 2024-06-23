d9b03bb0d9b9ead9a1e636c9e26cca281eb5ffee
naveen <naveenkumar9707@gmail.com>
2024-06-23 12:27:43 -0500

    for each loop to create servers

--- roboshop/servers.tf
+++ roboshop/servers.tf
@@ -66,10 +66,10 @@ variable "components" {
 resource "aws_instance" "instance" {
   for_each = var.components
   ami           = data.aws_ami.ami.image_id
-  instance_type = "${each.value["instance_type"]}"
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
   tags = {
-    Name= "${each.value["name"]}"
+    Name= each.value["name"]
   }
 }
 
