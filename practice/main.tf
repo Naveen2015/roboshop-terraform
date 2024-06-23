7bcd177d857bbf53a8ce931ac1d91a604c6e8528
naveen <naveenkumar9707@gmail.com>
2024-06-22 20:50:28 -0500

    terrraform variables

--- practice/main.tf
+++ practice/main.tf
@@ -9,3 +9,11 @@ output "result" {
 output "result1" {
   value = "the value of the variable is ${var.sample}"
 }
+
+variable "mylist" {
+  default = [naveen,0,reddy,test]
+}
+
+output "listvalues" {
+  value = "the value from list ${var.mylist[2]}"
+}
--- roboshop/servers.tf
+++ roboshop/servers.tf
@@ -4,73 +4,77 @@ data "aws_ami" "ami" {
   name_regex = "Centos-8-DevOps-Practice"
 }
 
+variable "instance_type" {
+  default = "t3.micro"
+}
+
 
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "frontend"
   }
 }
 resource "aws_instance" "mongod" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "mongod"
   }
 }
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "catalogue"
   }
 }
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "redis"
   }
 }
 resource "aws_instance" "user" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "user"
   }
 }
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "cart"
   }
 }
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "mysql"
   }
 }
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "shipping"
   }
 }
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "rabbitmq"
   }
 }
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     Name= "payment"
   }
